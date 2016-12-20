de3636a041bb4aaf6c22618e71904904f393d077
jrdn hannah <jrdn@jrdnhannah.co.uk>
2016-12-20 09:00:05 +0000

    [Run Script] App path can be set by env

--- run.sh
+++ run.sh
@@ -2,9 +2,10 @@
 
 QUEUE=${QUEUE:-default}
 CONNECTION=${CONNECTION:-default}
+LARAVEL_PATH=${APP:-/var/www}
 
-# Check for install of laravel at /var/www
-if ! [ -e /var/www/artisan ]
+# Check for install of laravel at $LARAVEL_PATH
+if ! [ -e ${LARAVEL_PATH}/artisan ]
 then
   echo "Laravel installation not found, exiting"
   exit 45
@@ -14,14 +15,14 @@ fi
 chown -R www-data:www-data /var/www/storage
 
 # Create the framework cache dirs
-mkdir -p /var/www/storage/app
-mkdir -p /var/www/storage/logs
-mkdir -p /var/www/storage/framework/sessions
-mkdir -p /var/www/storage/framework/views
-mkdir -p /var/www/storage/framework/cache
-chown -R www-data:www-data /var/www/storage/framework
-chown -R www-data:www-data /var/www/storage/logs
-chown -R www-data:www-data /var/www/storage/app
+mkdir -p ${LARAVEL_PATH}/storage/app
+mkdir -p ${LARAVEL_PATH}/storage/logs
+mkdir -p ${LARAVEL_PATH}/storage/framework/sessions
+mkdir -p ${LARAVEL_PATH}/storage/framework/views
+mkdir -p ${LARAVEL_PATH}/storage/framework/cache
+chown -R www-data:www-data ${LARAVEL_PATH}/storage/framework
+chown -R www-data:www-data ${LARAVEL_PATH}/storage/logs
+chown -R www-data:www-data ${LARAVEL_PATH}/storage/app
 
 # Start the php serve
-php /var/www/artisan queue:work ${CONNECTION} --queue=${QUEUE}
+php ${LARAVEL_PATH}/artisan queue:work ${CONNECTION} --queue=${QUEUE}
